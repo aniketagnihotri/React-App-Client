101fc66950010c8c12c7f542834ca24191944d47
aniketagnihotri <54214125+aniketagnihotri@users.noreply.github.com>
2020-07-10 23:34:56 -0400

    configuration of react project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter.jsx'
+
+const element = <h1>Hello World</h1>;
+ReactDOM.render(<Counter />, document.getElementById('root'))
